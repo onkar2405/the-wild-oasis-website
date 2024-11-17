3604b0e48d65af36cfb159d6a26f6c301d17f140
onkar2405 <onkarvaidyad789@gmail.com>
2024-11-17 22:48:37 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+    </div>
+  );
+}
